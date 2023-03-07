17f50a0b2fc563086448dc0c0630ee829b02b27b
Gblack-Hub <petershola18@gmail.com>
2023-03-07 14:18:04 +0100

    inputfield component.

---
+++ src/components/reusable/input-field/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+import { parseClassName } from "./../../../utils";
+
+interface IInputField {
+  name: string;
+  type: string;
+  label: string;
+  placeholder: string;
+  value: any;
+  onChange(e: React.ChangeEvent<HTMLInputElement>): void;
+  classes?: string;
+}
+
+const InputField = React.forwardRef(
+  (
+    { name, type, placeholder, label, classes, ...props }: IInputField,
+    ref: any
+  ) => {
+    return (
+      <input
+        className={parseClassName([
+          "w-100 py-2 px-3",
+          styles.field,
+          classes ? classes : "",
+        ])}
+        name={name}
+        id={name}
+        type={type}
+        placeholder={placeholder}
+        aria-label={label}
+        autoComplete="false"
+        autoCorrect="false"
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+export default InputField;
