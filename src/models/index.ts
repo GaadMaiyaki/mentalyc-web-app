9807f05534010bf3eeb3671470543ea908b93b9a
Gblack-Hub <petershola18@gmail.com>
2023-03-07 14:16:20 +0100

    interfaces for notes and input values.

---
+++ src/models/index.ts
@@ -0,0 +1,11 @@
+export interface INotes {
+  note_type: string;
+  client_name: string;
+  id: number;
+  progress: number;
+}
+
+export interface IInputValues {
+  note_type: string;
+  client_name: string;
+}
