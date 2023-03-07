53c71dfba0014d7007922f3f4ab09cb987ce490f
Gblack-Hub <petershola18@gmail.com>
2023-03-07 13:58:24 +0100

    tested app component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,13 @@
 import App from "./App";
 
+import { BrowserRouter } from "react-router-dom";
+
 import { render } from "@testing-library/react";
 
 test("should render without crashing", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 });
