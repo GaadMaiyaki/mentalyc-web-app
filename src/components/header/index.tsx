3c031a5ee4a110b4c937cb54e7f7e3bc01820442
Gblack-Hub <petershola18@gmail.com>
2023-03-07 14:17:39 +0100

    header component.

---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./index.module.scss";
+
+import { parseClassName } from "./../../utils";
+
+import LogoSvg from "../../assets/svgs/logo";
+
+const Header = () => {
+  return (
+    <section className={parseClassName(["py-3", styles.wrapper])}>
+      <div className="container">
+        <aside className="d-flex justify-content-center justify-content-lg-start justify-content-xl-start justify-content-xxl-start align-items-center">
+          <span aria-label="Mentalyc logo" title="Mentalyc logo">
+            <LogoSvg />
+          </span>
+        </aside>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
