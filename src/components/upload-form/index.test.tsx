d14834678a86ea3a9f8bb097e942dfce6fc088ec
Gblack-Hub <petershola18@gmail.com>
2023-03-07 14:09:08 +0100

    test cases for UploadFom component.

---
+++ src/components/upload-form/index.test.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+
+import UploadForm from ".";
+
+describe("UploadForm", () => {
+  it("should render correctly", () => {
+    const { getByText, getByLabelText } = render(
+      <UploadForm isSleeping={false} handleSubmit={() => {}} />
+    );
+
+    expect(getByText("Complete Your Upload")).toBeInTheDocument();
+    expect(
+      getByText("Fill in the details below to complete your upload")
+    ).toBeInTheDocument();
+    expect(getByLabelText("Client name")).toBeInTheDocument();
+    expect(getByLabelText("Note type")).toBeInTheDocument();
+    expect(getByText("Finish Upload")).toBeInTheDocument();
+  });
+
+  it("should disable button when client name and note type are not selected", () => {
+    const { getByRole, getByLabelText } = render(
+      <UploadForm isSleeping={false} handleSubmit={() => {}} />
+    );
+
+    expect(getByRole("button")).toBeDisabled();
+
+    const clientNameInput = getByLabelText("Client name");
+    const noteTypeInput = getByLabelText("Note type");
+
+    fireEvent.change(clientNameInput, { target: { value: "Peter" } });
+    expect(getByRole("button")).toBeDisabled();
+
+    fireEvent.change(noteTypeInput, {
+      target: { value: "Progress note - 80 left" },
+    });
+    expect(getByRole("button")).toBeEnabled();
+  });
+
+  it("should call handleSubmit on submit", () => {
+    const handleSubmit = jest.fn();
+    const { getByRole, getByLabelText } = render(
+      <UploadForm isSleeping={false} handleSubmit={handleSubmit} />
+    );
+
+    const clientNameInput = getByLabelText("Client name");
+    const noteTypeInput = getByLabelText("Note type");
+
+    const submitButton = getByRole("button", { name: "Finish Upload" });
+
+    fireEvent.change(clientNameInput, { target: { value: "Peter" } });
+    fireEvent.change(noteTypeInput, {
+      target: { value: "Progress note - 80 left" },
+    });
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      client_name: "Peter",
+      note_type: "Progress note - 80 left",
+    });
+  });
+
+  it("should show 'Please wait...' when form is submitting", async () => {
+    const handleSubmit = jest.fn();
+    const { getByRole } = render(
+      <UploadForm isSleeping={true} handleSubmit={handleSubmit} />
+    );
+    const submitButton = getByRole("button", { name: "Please wait..." });
+    expect(submitButton).toBeInTheDocument();
+  });
+});
