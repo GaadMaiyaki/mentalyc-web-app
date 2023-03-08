7572ecdc38a9e3902f3bd51dde2fb1915b10eb60
Gblack-Hub <petershola18@gmail.com>
2023-03-08 10:12:38 +0100

    updated.

--- src/components/reusable/list-view/index.test.tsx
+++ src/components/reusable/list-view/index.test.tsx
@@ -20,19 +20,19 @@ const notes = [
 ];
 
 describe("ListView component", () => {
-  test("should display header correctly with plural notes in progress", () => {
+  it("should display header correctly with plural notes in progress", () => {
     render(<ListView data={notes} setNotes={() => {}} />);
     expect(screen.getByText("2")).toBeInTheDocument();
     expect(screen.getByText("Notes in progress")).toBeInTheDocument();
   });
 
-  test("should display header correctly with singular note in progress", () => {
+  it("should display header correctly with singular note in progress", () => {
     render(<ListView data={[notes[0]]} setNotes={() => {}} />);
     expect(screen.getByText("1")).toBeInTheDocument();
     expect(screen.getByText("Note in progress")).toBeInTheDocument();
   });
 
-  test("should display notes list correctly", () => {
+  it("should display notes list correctly", () => {
     render(<ListView data={notes} setNotes={() => {}} />);
     expect(screen.getByText("Client")).toBeInTheDocument();
     expect(screen.getByText("Type")).toBeInTheDocument();
@@ -43,8 +43,8 @@ describe("ListView component", () => {
     expect(screen.getByText("EMDR note")).toBeInTheDocument();
   });
 
-  test("should display no notes message when no notes are available", () => {
-    render(<ListView data={[]} setNotes={() => {}} />);
-    expect(screen.getByText("No available notes.")).toBeInTheDocument();
+  it("renders null when note prop is empty", () => {
+    const { container } = render(<ListView data={[]} setNotes={() => {}} />);
+    expect(container.firstChild).toBeNull();
   });
 });
